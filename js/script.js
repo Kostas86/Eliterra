f4786811318b3d9a75dd5a925ccb64af09ea4943
kostas86 <serebrennikovnumberone@gmail.com>
2020-04-24 20:25:43 +0300

    24.04 20:23

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '460px',
+        slidesToShow: 1,
+    });
+});
