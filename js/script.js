3886c1a5474664f684ebb96c07832e0b20901e58
kostas86 <serebrennikovnumberone@gmail.com>
2020-05-07 20:03:07 +0300

    07.05 20:02

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
         dots: true,
         centerMode: true,
         slidesToShow: 1,
-        autoplay:true,
-        autoplaySpeed:3000,
-        variableWidth:true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        variableWidth: true,
 
     });
 });
@@ -15,11 +15,46 @@ $(document).ready(function () {
         arrows: false,
         dots: true,
         centerMode: false,
-        variableWidth:true,
+        variableWidth: true,
         slidesToShow: 4,
-        autoplay:true,
-        autoplaySpeed:3000,
-        adaptiveHeight:true,
-        variableWidth:false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        variableWidth: false,
     });
 });
+ymaps.ready(init);
+var myMap,
+    myPlacemarkOne,
+    myPlacemarkTwo;
+
+function init() {
+    myMap = new ymaps.Map('map', {
+        center: [52.34789823419812, 35.34420201918029],
+        zoom: 16
+    });
+    myMap.controls
+           .remove("rulerControl")
+           .remove("searchControl")
+           .remove("trafficControl")
+           .remove("geolocationControl")
+           .remove("routeEditor");
+    myMap.behaviors.disable([
+        // 'drag',
+        'scrollZoom'
+    ]);
+
+    myPlacemarkOne = new ymaps.Placemark([52.34615042628716,35.344413358213544], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/main_map_point.svg',
+        iconImageSize: [33, 47],
+    });
+    myMap.geoObjects.add(myPlacemarkOne);
+
+    myPlacemarkTwo = new ymaps.Placemark([52.349948771328286,35.344407993795514], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/main_map_point.svg',
+        iconImageSize: [33, 47],
+    });
+    myMap.geoObjects.add(myPlacemarkTwo);
+}
